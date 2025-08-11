c2981fe20814dbb92a8259d502f59afed2c0647b
OliverLundag <oliver@lapapa88.com>
2025-08-11 09:15:45 +0800

    Add System out

---
+++ src/main/java/com/studentms/Main.java
@@ -0,0 +1,8 @@
+package com.studentms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting Student Management System...");
+        System.out.println("Stopped Student Management System...");
+    }
+}
