6fe60dfc5f7e43b6dbea24c8be3bf32c3d1036d9
aspect <aspectjump.java@gmail.com>
2025-08-11 10:04:44 +0800

    Update Main Class with todo

--- src/main/java/com/studentms/Main.java
+++ src/main/java/com/studentms/Main.java
@@ -3,6 +3,11 @@ package com.studentms;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting Student Management System...");
+        try {
+            //We need to call and test the database connection here
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Stopped Student Management System...");
     }
 }
