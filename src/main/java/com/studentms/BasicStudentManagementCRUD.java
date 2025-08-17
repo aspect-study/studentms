082a04f8abd01033348ffa34a14d8860a62f145c
aspect <aspectjump.java@gmail.com>
2025-08-17 22:21:42 +0800

    Add new main class

--- src/main/java/com/studentms/BasicStudentManagementCRUD.java
+++ src/main/java/com/studentms/BasicStudentManagementCRUD.java
@@ -1,17 +1,12 @@
 package com.studentms;
 
-import com.studentms.config.DatabaseConfig;
-import com.studentms.dao.StudentDAO;
-import com.studentms.dao.impl.StudentDAOImpl;
-import com.studentms.exceptions.DatabaseException;
-import com.studentms.model.Student;
 import com.studentms.view.StudentManagementView;
 
 /**
  * Main class of the Student Management Application Demo
  * @author Aspect;
  */
-public class BasicStudentManagementDemo {
+public class BasicStudentManagementCRUD {
     public static void main(String[] args) {
 
             StudentManagementView studentManagementView = new StudentManagementView();
